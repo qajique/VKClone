aba91eb51f31bbcbfabd50e1e1291b92ba4f87fe
Kazhymurat <komirzhan@gmail.com>
2022-01-22 16:36:52 +0600

    Initial Commit

---
+++ VKClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKClone
+//
+//  Created by Kazhymurat on 22.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
